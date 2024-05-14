5da964a2e381bf5a67507bc0727673e81fa39748
Tyler Kelley <tylerzanekelley@gmail.com>
2024-05-14 13:39:54 -0500

    fixed some formatting and adding nvidia driver module

--- modules/amd-drivers.nix
+++ modules/amd-drivers.nix
@@ -9,14 +9,10 @@ let
   cfg = config.drivers.amdgpu;
 in
 {
-  # Declare what settings a user of this "hello.nix" module CAN SET.
   options.drivers.amdgpu = {
     enable = mkEnableOption "Enable AMD Drivers";
   };
 
-  # Define what other settings, services and resources should be active IF
-  # a user of this "hello.nix" module ENABLED this module 
-  # by setting "services.hello.enable = true;".
   config = mkIf cfg.enable {
     systemd.tmpfiles.rules = [ "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}" ];
     services.xserver.enable = true;
