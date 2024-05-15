6d5b772b19836c7dfeb126e1cf15a500d2be1886
Tyler Kelley <tylerzanekelley@gmail.com>
2024-05-15 15:30:35 -0500

    Now import mason so I actually have lsp completion lol

--- hosts/familypc/home.nix
+++ hosts/familypc/home.nix
@@ -168,77 +168,76 @@ in
 
   programs = {
     gh.enable = true;
-    neovim = 
-    let
-      toLua = str: "lua << EOF\n${str}\nEOF\n";
-      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-    in
-    {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      extraPackages = with pkgs; [
-        lua-language-server
-      ];
-      plugins = with pkgs.vimPlugins; [
-        alpha-nvim
-        auto-session
-        bufferline-nvim
-        dressing-nvim
-        indent-blankline-nvim
-        nvim-treesitter.withAllGrammars
-	      lualine-nvim
-        nvim-autopairs
-      	nvim-web-devicons
-        nvim-cmp
-        nvim-surround
-        nvim-lspconfig
-        cmp-nvim-lsp
-        cmp-buffer
-        luasnip
-        cmp_luasnip
-        friendly-snippets
-        lspkind-nvim
-        comment-nvim
-        nvim-ts-context-commentstring
-        {
-          plugin = dracula-nvim;
-          config = "colorscheme dracula";
-        }
-        plenary-nvim
-        neodev-nvim
-        luasnip
-	      telescope-nvim
-        todo-comments-nvim
-        nvim-tree-lua
-        telescope-fzf-native-nvim
-        vim-tmux-navigator
-        mason-nvim
-        mason-lspconfig-nvim
-      ];
-      extraLuaConfig = ''
-        ${builtins.readFile ../../config/nvim/options.lua}
-        ${builtins.readFile ../../config/nvim/keymaps.lua}
-        ${builtins.readFile ../../config/nvim/plugins/alpha.lua}
-        ${builtins.readFile ../../config/nvim/plugins/autopairs.lua}
-        ${builtins.readFile ../../config/nvim/plugins/auto-session.lua}
-        ${builtins.readFile ../../config/nvim/plugins/comment.lua}
-        ${builtins.readFile ../../config/nvim/plugins/cmp.lua}
-        ${builtins.readFile ../../config/nvim/plugins/lsp.lua}
-        ${builtins.readFile ../../config/nvim/plugins/nvim-tree.lua}
-        ${builtins.readFile ../../config/nvim/plugins/telescope.lua}
-        ${builtins.readFile ../../config/nvim/plugins/todo-comments.lua}
-        ${builtins.readFile ../../config/nvim/plugins/treesitter.lua}
-        require("ibl").setup()
-        require("bufferline").setup{}
-        require("lualine").setup({
-          icons_enabled = true,
-          theme = 'dracula',
-        })
-      '';
-    };
+    neovim =
+      let
+        toLua = str: "lua << EOF\n${str}\nEOF\n";
+        toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+      in
+      {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
+        extraPackages = with pkgs; [ lua-language-server ];
+        plugins = with pkgs.vimPlugins; [
+          alpha-nvim
+          auto-session
+          bufferline-nvim
+          dressing-nvim
+          indent-blankline-nvim
+          nvim-treesitter.withAllGrammars
+          lualine-nvim
+          nvim-autopairs
+          nvim-web-devicons
+          nvim-cmp
+          nvim-surround
+          nvim-lspconfig
+          cmp-nvim-lsp
+          cmp-buffer
+          luasnip
+          cmp_luasnip
+          friendly-snippets
+          lspkind-nvim
+          comment-nvim
+          nvim-ts-context-commentstring
+          {
+            plugin = dracula-nvim;
+            config = "colorscheme dracula";
+          }
+          plenary-nvim
+          neodev-nvim
+          luasnip
+          telescope-nvim
+          todo-comments-nvim
+          nvim-tree-lua
+          telescope-fzf-native-nvim
+          vim-tmux-navigator
+          mason-nvim
+          mason-lspconfig-nvim
+        ];
+        extraLuaConfig = ''
+          ${builtins.readFile ../../config/nvim/options.lua}
+          ${builtins.readFile ../../config/nvim/keymaps.lua}
+          ${builtins.readFile ../../config/nvim/plugins/alpha.lua}
+          ${builtins.readFile ../../config/nvim/plugins/autopairs.lua}
+          ${builtins.readFile ../../config/nvim/plugins/auto-session.lua}
+          ${builtins.readFile ../../config/nvim/plugins/comment.lua}
+          ${builtins.readFile ../../config/nvim/plugins/cmp.lua}
+          ${builtins.readFile ../../config/nvim/plugins/lsp.lua}
+          ${builtins.readFile ../../config/nvim/plugins/nvim-tree.lua}
+          ${builtins.readFile ../../config/nvim/plugins/mason.lua}
+          ${builtins.readFile ../../config/nvim/plugins/telescope.lua}
+          ${builtins.readFile ../../config/nvim/plugins/todo-comments.lua}
+          ${builtins.readFile ../../config/nvim/plugins/treesitter.lua}
+          require("ibl").setup()
+          require("bufferline").setup{}
+          require("lualine").setup({
+            icons_enabled = true,
+            theme = 'dracula',
+          })
+        '';
+      };
     kitty = {
       enable = true;
       package = pkgs.kitty;
