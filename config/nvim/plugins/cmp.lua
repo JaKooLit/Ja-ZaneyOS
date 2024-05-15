7001265426fe3b80767b594853e787c01e594c89
Tyler Kelley <tylerzanekelley@gmail.com>
2024-05-15 15:10:04 -0500

    Add lsp as source in autocompletion

--- config/nvim/plugins/cmp.lua
+++ config/nvim/plugins/cmp.lua
@@ -25,6 +25,7 @@ cmp.setup({
   }),
   -- sources for autocompletion
   sources = cmp.config.sources({
+    { name = "nvim_lsp" }, -- language servers
     { name = "luasnip" }, -- snippets
     { name = "buffer" }, -- text within current buffer
     { name = "path" }, -- file system paths
