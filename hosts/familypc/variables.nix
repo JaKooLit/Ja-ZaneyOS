42012cd6978eaea8768209e3dff854b1b3592252
Tyler Kelley <tylerzanekelley@gmail.com>
2024-05-21 14:19:59 -0500

    update theme and waybar icons

--- hosts/familypc/variables.nix
+++ hosts/familypc/variables.nix
@@ -4,7 +4,7 @@
   gitEmail = "tylerzanekelley@gmail.com";
 
   # Base16 Theme
-  theme = "dracula";
+  theme = "tokyo-night-storm";
 
   # Hyprland Settings
   borderAnim = true; # Enable / Disable Hyprland Border Animation
@@ -15,6 +15,6 @@
   waybarAnimations = true;
 
   # Program Options
-  browser = "firefox"; # Set Default Browser
+  browser = "firefox"; # Set Default Browser (google-chrome-stable for google-chrome)
   terminal = "kitty"; # Set Default System Terminal
 }
