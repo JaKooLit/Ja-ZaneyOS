389e02e6025611f742fe65395ad7b18109c26f44
redbeardymcgee <redbeardymcgee@gmail.com>
2024-07-13 17:32:16 -0500

    fastfetch: dedupe repetitive values

--- config/fastfetch/default.nix
+++ config/fastfetch/default.nix
@@ -1,101 +1,94 @@
 {
   programs.fastfetch = {
     enable = true;
+
     settings = {
-      logo = {
-          source = ./nixos.png;
-          type = "kitty-direct";
-          height = 15;
-          width = 30;
-          padding = {
-              top = 3;
-              left = 3;
-          };
+      display = {
+        color = {
+          keys = "35";
+          output = "90";
+        };
       };
+
+      logo = {
+        source = ./nixos.png;
+        type = "kitty-direct";
+        height = 15;
+        width = 30;
+        padding = {
+          top = 3;
+          left = 3;
+        };
+      };
+
       modules = [
           "break"
           {
               type = "custom";
               format = "┌──────────────────────Hardware──────────────────────┐";
-              outputColor = "90";
           }
           {
               type = "cpu";
-              key = "│ ";
-              keyColor = "35";
+              key = "│  ";
           }
           {
               type = "gpu";
-              key = "│ 󰍛";
-              keyColor = "35";
+              key = "│ 󰍛 ";
           }
           {
               type = "memory";
-              key = "│ 󰑭";
-              keyColor = "35";
+              key = "│ 󰑭 ";
           }
           {
               type = "custom";
               format = "└────────────────────────────────────────────────────┘";
-              outputColor = "90";
           }
           "break"
           {
               type = "custom";
               format = "┌──────────────────────Software──────────────────────┐";
-              outputColor = "90";
           }
           {
               type = "custom";
               format = " OS -> ZaneyOS 2.2";
-              outputColor = "60";
           }
           {
               type = "kernel";
-              key = "│ ├";
-              keyColor = "31";
+              key = "│ ├ ";
           }
           {
               type = "packages";
-              key = "│ ├󰏖";
-              keyColor = "31";
+              key = "│ ├󰏖 ";
           }
           {
               type = "shell";
-              key = "└ └";
-              keyColor = "31";
+              key = "└ └ ";
           }
           "break"
           {
               type = "wm";
               key = " WM";
-              keyColor = "32";
           }
           {
               type = "wmtheme";
-              key = "│ ├󰉼";
-              keyColor = "32";
+              key = "│ ├󰉼 ";
           }
           {
               type = "terminal";
-              key = "└ └";
-              keyColor = "32";
+              key = "└ └ ";
           }
           {
               type = "custom";
               format = "└────────────────────────────────────────────────────┘";
-              outputColor = "90";
           }
           "break"
           {
               type = "custom";
               format = "┌────────────────────Uptime / Age────────────────────┐";
-              outputColor = "90";
           }
           {
               type = "command";
-              key = "│ ";
-              keyColor = "33";
+              key = "│  ";
               text = #bash
               ''
                 birth_install=$(stat -c %W /)
@@ -107,13 +100,11 @@
           }
           {
               type = "uptime";
-              key = "│ ";
-              keyColor = "33";
+              key = "│  ";
           }
           {
               type = "custom";
               format = "└────────────────────────────────────────────────────┘";
-              outputColor = "90";
           }
           "break"
       ];
